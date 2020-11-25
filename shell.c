eda2952ee08b8b30294c659a73bc8cd6d4429bd5
Nikki Sotak <nsotak@colgate.edu>
2020-11-25 14:13:26 -0500

    Worked on part1. Wrote create_process function.

--- shell.c
+++ shell.c
@@ -7,6 +7,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+//We included:
+#include <sys/wait.h>
+#include <errno.h>
+#include <unistd.h>
+
+
 #define PROMPT "shell> "
 #define MAX_BACKGROUND 50
 
@@ -58,16 +64,44 @@ void free_tokens(char **tokens) {
     }
     free(tokens); // then free the array
 }
+
+
+int create_process(char **command) {
+    pid_t child_pid;
+    int ret;
+
+    child_pid = fork();
+
+	if (child_pid == 0) { 
+		ret = execv(command[0], command);//execv only returns -1 if an error has occurred 
+		fprintf(stderr, "execv failed: %s\n", strerror(errno)); 
+        //might create helper function that handles other types of errors. (Defensive programming);
+	}
+	else {
+		int child_status; 
+        waitpid(child_pid, &child_status, 0);
+		printf("Child finished\n"); //DEBUGGER
+	}
+    return child_pid;
+}
+
+
+
 int main(int argc, char **argv) {
     // main loop for the shell
     printf("%s", PROMPT);
     fflush(stdout);  // Display the prompt immediately
     char buffer[1024];
     while (fgets(buffer, 1024, stdin) != NULL) {
-        char **command = tokenize(buffer, " \t\n");
-
-        // TODO: run the command
-
+        char **command = tokenize(buffer, " \t\n"); //command[0] holds "/bin/ls"; command[1] holds "-l"
+        printf("%s \n", command[0]);//DEBUGGER
+        if (strncmp(command[0], "exit", 4) == 0) {  //check whether built-in or exit() command
+           exit(0); 
+        }
+        else {
+            int child_pid = create_process(command);
+            //exit(child_pid); may not be necessary. Uncommenting it will cause shell to exit while loop
+        }
         printf("%s", PROMPT);
         fflush(stdout);  // Display the prompt immediately
     }
