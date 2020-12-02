e4f2c247feba54fb1f7dbffb2dc4f67312cbdf90
Nikki Sotak <nsotak@colgate.edu>
2020-12-02 10:29:43 -0500

    Wrote code checking for current active processes and helper function to create background processes.

--- shell.c
+++ shell.c
@@ -81,7 +81,21 @@ int create_process(char **command) {
 	else {
 		int child_status; 
         waitpid(child_pid, &child_status, 0);
-		printf("Child finished\n"); //DEBUGGER
+	}
+    return child_pid;
+}
+
+int create_back_process(char **command) {
+    pid_t child_pid;
+    int ret;
+
+    child_pid = fork();
+
+	if (child_pid == 0) { 
+		ret = execv(command[0], command);//execv only returns -1 if an error has occurred 
+		fprintf(stderr, "execv failed: %s\n", strerror(errno)); 
+        //might create helper function that handles other types of errors. (Defensive programming);
+        exit(0); //exit failed process
 	}
     return child_pid;
 }
@@ -93,15 +107,28 @@ int main(int argc, char **argv) {
     printf("%s", PROMPT);
     fflush(stdout);  // Display the prompt immediately
     char buffer[1024];
+    unsigned int active_PID_number = 0; //keep track of # of active processes
+    unsigned int PIDS_IN_PROCESS[MAX_BACKGROUND]; //Moved here b/c needs to be declared before check
     while (fgets(buffer, 1024, stdin) != NULL) {
-        // create check for runnning processes
-        // if finished do print statment
-        // remove ## from the list
-        // active_PID_number--
+        for (int i = 0; i < active_PID_number; i++) {// check for runnning processes
+            int child_pid = PIDS_IN_PROCESS[i];
+            int child_status;
+            int ret = waitpid(child_pid, &child_status, WNOHANG);
+            if ( ret > 0) {// if finished do print statment
+                printf("%d finished with exit code %d\n", child_pid, child_status);
+                // remove ## from the list
+                active_PID_number--;// active_PID_number--
+            }
+            else if (ret == -1) {
+                fprintf(stderr, "execv failed: %s\n", strerror(errno)); 
+            }
+        }
         char **command = tokenize(buffer, " \t\n"); //command[0] holds "/bin/ls"; command[1] holds "-l"
         printf("Beginning is %s \n", command[0]);//DEBUGGER
-        unsigned int PIDS_IN_PROCESS[MAX_BACKGROUND];
-        unsigned int active_PID_number = 0;
+        /*Moved these declarations outside while loop;
+        unsigned int PIDS_IN_PROCESS[MAX_BACKGROUND]; 
+        unsigned int active_PID_number = 0;*/
+
         //check if nothing was entered
         if (command[0] != NULL){
             // Get what the last char is
@@ -118,24 +145,28 @@ int main(int argc, char **argv) {
                 // This should work if the bottom is set
                 // Need to work in out to include the ,
                 printf("Process currently active: ");
-                for (int x = 0; x < active_PID_number; x ++) {
-                    printf("%d ", PIDS_IN_PROCESS[x]);
+                for (int x = 0; x < (active_PID_number - 1); x++) {
+                    printf("%d, ", PIDS_IN_PROCESS[x]);
                 }
-                printf("\n");
+                printf("%d\n", PIDS_IN_PROCESS[active_PID_number - 1]);
             } else if (strncmp(command[0], "kill", 4) == 0) { //kill pid in process
                 printf("ITS KILL PID\n");
                 // use kill + extras found in instr
                 // take ## out of the list
                 // active_PID_number--
             } else if (strncmp(command[i-1], "&", 4) == 0){
-                printf("ITS AND &\n");
-                // create process
-                // add ## to the list (PIDS_IN_PROCESS)
-                // increase active_PID_number by one
+                printf("ITS AND &\n"); //DEBUGGER
+                printf("Active process count before %s is created: %d\n", command[0], active_PID_number);  //DEBUGGER
+                int child_pid = create_back_process(command);
+                printf("New PID: %d\n", child_pid); //DEBUGGER
+                PIDS_IN_PROCESS[active_PID_number] = child_pid;// add ## to the list (PIDS_IN_PROCESS)
+                active_PID_number++;// increase active_PID_number by one
+                printf("Active process count after %s is created: %d\n", command[0], active_PID_number);  //DEBUGGER
+
             } else {
                 // printf("IS NOT NULL \n"); //DEBUGGER
                 int child_pid = create_process(command);
-                //exit(child_pid); may not be necessary. Uncommenting it will cause shell to exit while loop
+                //might have to declare child status variable here
             }
         }
 
