e4f834c713ada1f13100ec20008422022e445777
Nikki Sotak <nsotak@colgate.edu>
2020-12-04 17:45:00 -0500

    Completed the project. All tests passed.

--- shell.c
+++ shell.c
@@ -90,7 +90,6 @@ int create_process(char **command) {
 }
 
 int create_back_process(char **command) {
-    //
     pid_t pid;
     int ret;
 
@@ -105,6 +104,22 @@ int create_back_process(char **command) {
     return pid;
 }
 
+int * remove_process(int* PIDS_IN_PROCESS, int pid, int num_active) {
+
+    int active_PID_number = num_active;
+    int y;
+    int found = 0;
+    for (y = 0; y < active_PID_number - 1; y++){
+        if (pid == PIDS_IN_PROCESS[y]) {
+              found = 1;
+        }
+        if (found == 1) { 
+            PIDS_IN_PROCESS[y] = PIDS_IN_PROCESS[y + 1];
+        }
+    }
+    return PIDS_IN_PROCESS;
+}
+
 
 
 
@@ -114,32 +129,26 @@ int main(int argc, char **argv) {
     fflush(stdout);  // Display the prompt immediately
     char buffer[1024];
     int active_PID_number = 0; //keep track of # of active processes
-    unsigned int PIDS_IN_PROCESS[MAX_BACKGROUND]; //Moved here b/c needs to be declared before check
+    int PIDS_IN_PROCESS[MAX_BACKGROUND]; //Moved here b/c needs to be declared before check
     while (fgets(buffer, 1024, stdin) != NULL) {
         // check for runnning processes
-        // Alternative Idea: Set a boolean var. to true when a background process is running. 
-        /*
+        
         for (int i = 0; i < active_PID_number; i++) {
             int child_pid = PIDS_IN_PROCESS[i];
-            int child_status;
-            printf("Child status: %d\n", child_status); 
+            int child_status = 0;
             int ret = waitpid(child_pid, &child_status, WNOHANG);
             if (ret > 0) {// if finished do print statment
                 printf("%d finished with exit code %d\n", child_pid, child_status);
-                // remove ## from the list
+                remove_process(PIDS_IN_PROCESS,child_pid, active_PID_number); // remove ## from the list
                 active_PID_number--;// active_PID_number--
             }
             else if (ret == -1) {
                 fprintf(stderr, "execv failed: %s\n", strerror(errno)); 
             }
-        }*/
+        }
 
         char **command = tokenize(buffer, " \t\n"); //command[0] holds "/bin/ls"; command[1] holds "-l"
-        printf("Beginning is %s \n", command[0]);//DEBUGGER
-        /*Moved these declarations outside while loop;
-        unsigned int PIDS_IN_PROCESS[MAX_BACKGROUND]; 
-        unsigned int active_PID_number = 0;*/
-
+        
         //check if nothing was entered
         if (command[0] != NULL){
             // Get what the last char is
@@ -147,51 +156,27 @@ int main(int argc, char **argv) {
             while(command[i] != NULL) {
                 i++;
             }
-            printf("The last char is %s\n", command[i-1]);
-            ///////////////////////////
             if (strncmp(command[0], "exit", 4) == 0) {  //check whether built-in or exit() command
                 exit(0); 
-            } 
-            else if (strncmp(command[0], "jobs", 10) == 0) { //print number of jobs
-                // printf("ITS JOBS\n"); //DEBUGGER
-                // This should work if the bottom is set
-                // Need to work in out to include the ,
-                printf("Process currently active (should print %d): ", active_PID_number);
+            } else if (strncmp(command[0], "jobs", 10) == 0) { //print number of jobs
+                printf("Process currently active: ");
                 int max = active_PID_number - 1;
                 for (int x = 0; x < max; x++) {
                     printf("%d, ", PIDS_IN_PROCESS[x]);
                 }
                 printf("%d\n", PIDS_IN_PROCESS[max]);
             } else if (strncmp(command[0], "kill", 4) == 0) { //kill pid in process
-                printf("ITS KILL PID\n");
-                pid_t child_pid = getpid();
-                if (child_pid == atoi(command[1])) {
-                    printf("Its the same");
-                    kill(child_pid, SIGKILL); // use kill 
-                }
-                //for loop to take it out of the list
-                int y = 0;
-                // for (int y = 0; y < active_PID_number; y++){
-                //     if (command[1] == PIDS_IN_PROCESS[y]) {
-                //         PIDS_IN_PROCESS[y] = NULL;
-                //     }
-                // }
-                // for(y; y < active_PID_number; y++) {
-                //     PIDS_IN_PROCESS[y] = PIDS_IN_PROCESS[y + 1]
-                // }
+                pid_t child_pid = atoi(command[1]);
+                kill(child_pid, SIGKILL); // use kill 
+                remove_process(PIDS_IN_PROCESS, child_pid, active_PID_number);
                 active_PID_number--; // active_PID_number--
             } else if (strncmp(command[i-1], "&", 4) == 0){
-                printf("ITS AND &\n"); //DEBUGGER
-                printf("Active process count before %s is created: %d\n", command[0], active_PID_number);  //DEBUGGER
-                command[i-1] = NULL;
+                command[i-1] = NULL; 
                 int child_pid = create_back_process(command);
-                printf("New PID: %d\n", child_pid); //DEBUGGER
                 PIDS_IN_PROCESS[active_PID_number] = child_pid;// add ## to the list (PIDS_IN_PROCESS)
                 active_PID_number++;// increase active_PID_number by one
-                printf("Active process count after %s is created: %d\n", command[0], active_PID_number);  //DEBUGGER
             } else {
-                // printf("IS NOT NULL \n"); //DEBUGGER
-                int child_pid = create_process(command);
+                create_process(command);
             }
         }
         printf("%s", PROMPT);
